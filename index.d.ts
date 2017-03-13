2cfe5bff16a38568efdad702f902f643155092a8
Sam Elsamman <sam.elsamman@gmail.com>
2017-03-13 16:04:37 -0400

    case issue

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export {Persistable, Persistor} from './lib/Persistable';
+export {Persistable, Persistor} from './lib/persistable';
 export {Supertype} from 'supertype';
 export function property(props?: Object);
 export function supertypeClass(target?: Function);
